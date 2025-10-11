ace8b03aeb7a2dd3a60a774214c6a37ad6c8e9fc
Alexei Surkov <alexei.y.surkov@gmail.com>
2025-10-11 17:08:45 +0300

    ArcadeDB lesson

--- ok-marketplace-tests/ok-marketplace-e2e-be/src/test/kotlin/TestGrJvm.kt
+++ ok-marketplace-tests/ok-marketplace-e2e-be/src/test/kotlin/TestGrJvm.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.TestInstance
 import ru.otus.otuskotlin.marketplace.e2e.be.base.BaseContainerTest
 import ru.otus.otuskotlin.marketplace.e2e.be.base.client.Client
 import ru.otus.otuskotlin.marketplace.e2e.be.base.client.RestClient
-import ru.otus.otuskotlin.marketplace.e2e.be.docker.KtorJvmGRDockerCompose
+import ru.otus.otuskotlin.marketplace.e2e.be.docker.KtorJvmGrDockerCompose
 import ru.otus.otuskotlin.marketplace.e2e.be.scenarios.v1.ScenariosV1
 import ru.otus.otuskotlin.marketplace.e2e.be.scenarios.v2.ScenariosV2
 import ru.otus.otuskotlin.marketplace.api.v1.models.AdDebug as AdDebugV1
@@ -15,7 +15,7 @@ import ru.otus.otuskotlin.marketplace.api.v2.models.AdDebug as AdDebugV2
 import ru.otus.otuskotlin.marketplace.api.v2.models.AdRequestDebugMode as AdRequestDebugModeV2
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-class TestGrJvm : BaseContainerTest(KtorJvmGRDockerCompose) {
+class TestGrJvm : BaseContainerTest(KtorJvmGrDockerCompose) {
     private val client: Client = RestClient(compose)
 
     @Test
