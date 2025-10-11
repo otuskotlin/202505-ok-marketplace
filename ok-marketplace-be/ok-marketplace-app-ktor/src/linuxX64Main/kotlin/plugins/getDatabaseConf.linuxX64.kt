c6d080cabdc48e4e430165feeffe502968fd69c5
Alexei Surkov <alexei.y.surkov@gmail.com>
2025-10-11 15:54:14 +0300

    ArcadeDB lesson

--- ok-marketplace-be/ok-marketplace-app-ktor/src/linuxX64Main/kotlin/plugins/getDatabaseConf.linuxX64.kt
+++ ok-marketplace-be/ok-marketplace-app-ktor/src/linuxX64Main/kotlin/plugins/getDatabaseConf.linuxX64.kt
@@ -2,13 +2,16 @@ package ru.otus.otuskotlin.marketplace.app.ktor.plugins
 
 import io.ktor.server.application.*
 import ru.otus.otuskotlin.marketplace.app.ktor.configs.ConfigPaths
+import ru.otus.otuskotlin.marketplace.app.ktor.configs.PostgresConfig
+import ru.otus.otuskotlin.marketplace.backend.repo.postgresql.RepoAdSql
+import ru.otus.otuskotlin.marketplace.backend.repo.postgresql.SqlProperties
 import ru.otus.otuskotlin.marketplace.common.repo.IRepoAd
 
 actual fun Application.getDatabaseConf(type: AdDbType): IRepoAd {
     val dbSettingPath = "${ConfigPaths.repository}.${type.confName}"
     return when (val dbSetting = environment.config.propertyOrNull(dbSettingPath)?.getString()?.lowercase()) {
         "in-memory", "inmemory", "memory", "mem" -> initInMemory()
-//        "postgres", "postgresql", "pg", "sql", "psql" -> initPostgres()
+        "postgres", "postgresql", "pg", "sql", "psql" -> initPostgres()
         else -> throw IllegalArgumentException(
             "$dbSettingPath has value of '$dbSetting', but it must be set in application.yml to one of: " +
                     "'inmemory', 'postgres'"
@@ -16,16 +19,16 @@ actual fun Application.getDatabaseConf(type: AdDbType): IRepoAd {
     }
 }
 
-//fun Application.initPostgres(): IRepoAd {
-//    val config = PostgresConfig(environment.config)
-//    return RepoAdSql(
-//        properties = SqlProperties(
-//            host = config.host,
-//            port = config.port,
-//            user = config.user,
-//            password = config.password,
-//            schema = config.schema,
-//            database = config.database,
-//        ),
-//    )
-//}
+fun Application.initPostgres(): IRepoAd {
+    val config = PostgresConfig(environment.config)
+    return RepoAdSql(
+        properties = SqlProperties(
+            host = config.host,
+            port = config.port,
+            user = config.user,
+            password = config.password,
+            schema = config.schema,
+            database = config.database,
+        ),
+    )
+}
